551c0dfdf9756a3e6b25407d219ef112a4954640
Joshua Tuddenham <joshuaisaact@gmail.com>
2025-10-27 05:57:18 +0000

    fix: robots.txt

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -4,6 +4,24 @@ const getRobotsTxt = (sitemapURL: URL) => `\
 User-agent: *
 Allow: /
 
+User-agent: GPTBot
+Disallow: /
+
+User-agent: ChatGPT-User
+Disallow: /
+
+User-agent: CCBot
+Disallow: /
+
+User-agent: Google-Extended
+Disallow: /
+
+User-agent: anthropic-ai
+Disallow: /
+
+User-agent: Claude-Web
+Disallow: /
+
 Sitemap: ${sitemapURL.href}
 `;
 
